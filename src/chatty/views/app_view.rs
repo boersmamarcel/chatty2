39eb8e60087703c8993628cb27ee5acba68286cf
Marcel Boersma <boersma.marcel@gmail.com>
2026-01-10 20:48:13 +0100

    Adds background color as theme

--- src/chatty/views/app_view.rs
+++ src/chatty/views/app_view.rs
@@ -1,8 +1,12 @@
 use crate::chatty::controllers::ChattyApp;
 use gpui::*;
+use gpui_component::ActiveTheme as _;
 
 impl Render for ChattyApp {
-    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
-        div().size_full().bg(rgb(0x1e1e1e)).child("Hello, Chatty!")
+    fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .size_full()
+            .bg(cx.theme().background)
+            .child("Hello, Chatty!")
     }
 }
