c2ebc9d789051ec69faf3bc9d8141126f1958425
boersmamarcel <boersma.marcel@gmail.com>
2026-01-10 13:51:52 +0100

    Initial empty project

---
+++ src/chatty/views/app_view.rs
@@ -0,0 +1,8 @@
+use crate::controllers::ChattyApp;
+use gpui::*;
+
+impl Render for ChattyApp {
+    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+        div().size_full().bg(rgb(0x1e1e1e)).child("Hello, Chatty!")
+    }
+}
