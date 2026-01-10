0781d014d5310e234f37bfe8a42acc6b78ae6c2e
boersmamarcel <boersma.marcel@gmail.com>
2026-01-10 18:53:47 +0100

    Initial settings view

--- src/settings/views/settings_view.rs
+++ src/settings/views/settings_view.rs
@@ -1,11 +1,46 @@
 use crate::settings::controllers::SettingsView;
 use gpui::*;
 
+use gpui_component::{
+    button::Button,
+    group_box::GroupBoxVariant,
+    h_flex,
+    label::Label,
+    setting::{
+        NumberFieldOptions, RenderOptions, SettingField, SettingFieldElement, SettingGroup,
+        SettingItem, SettingPage, Settings,
+    },
+    v_flex, ActiveTheme, Icon, IconName, Sizable, Size, Theme, ThemeMode,
+};
+
 impl Render for SettingsView {
-    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
-        div()
-            .size_full()
-            .bg(rgb(0x1e1e1e))
-            .child("Hello, Settings!")
+    fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        let view = cx.entity();
+
+        Settings::new("app-settings")
+            .with_size(Size::default())
+            .with_group_variant(GroupBoxVariant::Outline)
+            .pages(vec![SettingPage::new("General")
+                .resettable(true)
+                .default_open(true)
+                .groups(vec![SettingGroup::new().title("Appearance").items(
+                    vec![SettingItem::new(
+                        "Dark Mode",
+                        SettingField::switch(
+                            |cx: &App| cx.theme().mode.is_dark(),
+                            |val: bool, cx: &mut App| {
+                                let mode = if val {
+                                    ThemeMode::Dark
+                                } else {
+                                    ThemeMode::Light
+                                };
+                                Theme::global_mut(cx).mode = mode;
+                                Theme::change(mode, None, cx);
+                            },
+                        )
+                        .default_value(false),
+                    )
+                    .description("Switch between light and dark themes.")],
+                )])])
     }
 }
