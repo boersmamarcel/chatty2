f42eb82a5dcee34ccc236690a9f4db3b99c1a6f0
Marcel Boersma <boersma.marcel@gmail.com>
2026-02-03 19:28:43 +0100

    Adds many icons, removes unused settings

--- src/settings/models/general_model.rs
+++ src/settings/models/general_model.rs
@@ -4,7 +4,6 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Serialize, Deserialize)]
 pub struct GeneralSettingsModel {
     pub font_size: f32,
-    pub line_height: f32,
     pub theme_name: Option<String>,
     pub dark_mode: Option<bool>,
 }
@@ -13,7 +12,6 @@ impl Default for GeneralSettingsModel {
     fn default() -> Self {
         Self {
             font_size: 14.0,
-            line_height: 20.0,
             theme_name: None,
             dark_mode: None,
         }
